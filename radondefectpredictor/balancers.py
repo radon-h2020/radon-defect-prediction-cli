9305b038fcbb87bbdd053f2e1e212dfaded81891
Stefano Dalla Palma <stefano.dallapalma0@gmail.com>
2020-10-14 18:57:23 +0200

    first docs

--- radondefectpredictor/balancers.py
+++
@@ -1,8 +0,0 @@
-from imblearn.under_sampling import RandomUnderSampler
-from imblearn.over_sampling import RandomOverSampler
-
-balancers_map = dict(
-    none=None,
-    rus=RandomUnderSampler(sampling_strategy='majority', random_state=42),
-    ros=RandomOverSampler(sampling_strategy='minority', random_state=42)
-)
--- radondefectpredictor/normalizers.py
+++
@@ -1,7 +0,0 @@
-from sklearn.preprocessing import MinMaxScaler, StandardScaler
-
-normalizers_map = dict(
-    none=None,
-    minmax=MinMaxScaler(),
-    std=StandardScaler()
-)
