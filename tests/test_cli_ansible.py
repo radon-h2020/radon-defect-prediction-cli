700856cec3b57674c95a10e246719d62a0fcc960
stefanodallapalma <stefano.dallapalma0@gmail.com>
2021-03-24 10:55:08 +0100

    Added test cases

--- tests/test_cli_ansible.py
+++ tests/test_cli_ansible.py
@@ -4,6 +4,9 @@ import os
 import shutil
 import unittest
 
+from argparse import Namespace
+from radondp.cli import train, model as download_model, predict
+
 
 class CLIAnsibleTestCase(unittest.TestCase):
 
@@ -25,49 +28,147 @@ class CLIAnsibleTestCase(unittest.TestCase):
         os.mkdir(cls.download_model_dir)
         os.mkdir(cls.predict_dir)
 
-        os.system('cp {0} {1}/radondp_model.joblib'.format(
-            os.path.join(os.getcwd(), "test_data", "radondp_model_ansible.joblib"),
-            cls.predict_dir))
-
     @classmethod
     def tearDownClass(cls):
         shutil.rmtree(cls.workdir)
 
+    def test_train_invalid_balancer(self):
+        args = Namespace(
+            balancers=['none', 'WRONG'],
+            classifiers=['nb'],
+            path_to_csv=self.training_data_csv
+        )
+
+        with self.assertRaises(ValueError):
+            train(args)
+
+    def test_train_invalid_normalizer(self):
+        args = Namespace(
+            normalizers=['none', 'WRONG'],
+            classifiers=['nb'],
+            path_to_csv=self.training_data_csv
+        )
+
+        with self.assertRaises(ValueError):
+            train(args)
+
+    def test_train_invalid_classifier(self):
+        args = Namespace(
+            classifiers=['WRONG'],
+            path_to_csv=self.training_data_csv
+        )
+
+        with self.assertRaises(ValueError):
+            train(args)
+
+    def test_train_no_classifier(self):
+        args = Namespace(
+            path_to_csv=self.training_data_csv
+        )
+
+        with self.assertRaises(RuntimeError):
+            train(args)
+
     def test_train(self):
-        command = """
-        cd {0} && radon-defect-predictor train {1} "nb" -b "none rus ros" -n "none minmax std"
-        """.format(self.train_dir, self.training_data_csv)
+        args = Namespace(
+            balancers = ['none', 'rus', 'ros'],
+            normalizers=['none', 'minmax', 'std'],
+            classifiers=['nb'],
+            path_to_csv=self.training_data_csv
+        )
 
-        result = os.system(command)
+        try:
+            train(args)
+        except SystemExit as exc:
+            assert exc.code == 0
+            shutil.move('./radondp_model.joblib', os.path.join(self.train_dir, "radondp_model.joblib"))
 
-        assert (0 == result)
-        assert 'radondp_model.joblib' in os.listdir(self.train_dir)
-        model = joblib.load(os.path.join(self.train_dir, 'radondp_model.joblib'), mmap_mode='r')
-        assert model['estimator']
-        assert model['selected_features']
-        assert model['report']
+            assert 'radondp_model.joblib' in os.listdir(self.train_dir)
 
-    def test_model(self):
-        command = 'cd {0} && radon-defect-predictor download-model ansible github ANXS/postgresql'.format(
-            self.download_model_dir)
-        result = os.system(command)
-        assert (0 == result)
+            model = joblib.load(os.path.join(self.train_dir, 'radondp_model.joblib'), mmap_mode='r')
+            assert model['estimator']
+            assert model['selected_features']
+            assert model['report']
 
-        model = joblib.load(os.path.join(self.download_model_dir, 'radondp_model.joblib'), mmap_mode='r')
-        assert model['estimator']
-        assert model['selected_features']
+    def test_download_model_no_language(self):
+        args = Namespace()
+
+        try:
+            download_model(args)
+        except SystemExit as exc:
+            assert exc.code != 0
+
+    def test_download_model_wrong_language(self):
+        args = Namespace(language='wrong-language')
+
+        try:
+            download_model(args)
+        except SystemExit as exc:
+            assert exc.code != 0
+
+    def test_download_model(self):
+        args = Namespace(language='ansible')
+
+        try:
+            download_model(args)
+        except SystemExit as exc:
+            assert exc.code == 0
+            shutil.move('./radondp_model.joblib', os.path.join(self.download_model_dir, "radondp_model.joblib"))
+
+            model = joblib.load(os.path.join(self.download_model_dir, 'radondp_model.joblib'), mmap_mode='r')
+            assert model['estimator']
+            assert model['selected_features']
+
+    def test_predict_no_language(self):
+        args = Namespace(path_to_artefact=self.playbook)
+
+        try:
+            predict(args)
+        except SystemExit as exc:
+            assert exc.code != 0
+
+    def test_predict_wrong_language(self):
+        args = Namespace(language='wrong-language', path_to_artefact=self.playbook)
+
+        try:
+            predict(args)
+        except SystemExit as exc:
+            assert exc.code != 0
+
+    def test_predict_no_artefact_path(self):
+        args = Namespace(language='ansible')
+
+        try:
+            predict(args)
+        except SystemExit as exc:
+            assert exc.code != 0
 
     def test_predict(self):
-        command = 'cd {0} && radon-defect-predictor predict ansible {1}'.format(self.predict_dir, self.playbook)
-        result = os.system(command)
+        args = Namespace(language='ansible', path_to_artefact=self.playbook)
 
-        assert (0 == result)
+        try:
+            shutil.copy(os.path.join(os.getcwd(), "test_data", "radondp_model_ansible.joblib"),
+                        os.path.join(os.getcwd(), 'radondp_model.joblib'))
 
-        with open(os.path.join(self.predict_dir, 'radondp_predictions.json'), 'r') as f:
-            predictions = json.load(f)
-            assert len(predictions) == 1
-            assert predictions[0]['file'] == self.playbook
-            assert type(predictions[0]['failure_prone']) == bool
+            predict(args)
+        except SystemExit as exc:
+
+            assert exc.code == 0
+
+            shutil.move(os.path.join(os.getcwd(), 'radondp_model.joblib'),
+                        os.path.join( self.predict_dir, 'radondp_model_ansible.joblib'))
+
+            shutil.move(os.path.join(os.getcwd(), 'radondp_predictions.json'),
+                        os.path.join( self.predict_dir, 'radondp_predictions.json'))
+
+            with open(os.path.join(self.predict_dir, 'radondp_predictions.json'), 'r') as f:
+                predictions = json.load(f)
+                assert len(predictions) == 1
+                assert predictions[0]['file'] == self.playbook
+                assert type(predictions[0]['failure_prone']) == bool
+
+
+    """
 
     def test_model_and_predict(self):
         get_model = 'cd {0} && radon-defect-predictor download-model ansible github ANXS/postgresql'.format(
@@ -83,6 +184,7 @@ class CLIAnsibleTestCase(unittest.TestCase):
             predictions = json.load(f)
             assert len(predictions) == 1
 
+    """
 
 if __name__ == '__main__':
     unittest.main()
